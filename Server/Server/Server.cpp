6405f4495bef08d87fdb7ff4cc477109a63ee21b
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2021-11-21 02:42:48 +0900

    Revert "Merge remote-tracking branch 'origin/SH'"
    This reverts commit 4737889aa39f7de243c6f6aabdf1d3474a3b8e8e.

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -4,7 +4,6 @@
 
 Server::Server()
 {
-	GameThread = CreateThread(NULL, 0, GameThread, NULL, 0, NULL);
 	StartFlag = false;
 }
 
@@ -141,8 +140,8 @@ DWORD WINAPI Server::InitServer()
 				}
 
 
-				PlayerThread = CreateThread(NULL, 0, ProcessThread, (LPVOID)Wait_Queue[i], 0, NULL);
-				if (PlayerThread == NULL) closesocket(c_socket);
+				PThread = CreateThread(NULL, 0, ProcessThread, (LPVOID)Wait_Queue[i], 0, NULL);
+				if (PThread == NULL) closesocket(c_socket);
 
 
 			}
