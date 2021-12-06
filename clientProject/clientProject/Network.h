afb2482e0a4d71fb276cf3f40be3b540e4ae3de2
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2021-12-06 11:41:33 +0900

    Remote

--- clientProject/clientProject/Network.h
+++ clientProject/clientProject/Network.h
@@ -4,8 +4,10 @@
 
 //#define SERVERIP "192.168.143.240"
 //#define SERVERIP "192.168.219.100"
-#define SERVERIP "192.168.35.46"
+#define SERVERIP "192.168.20.50"
+//#define SERVERIP "192.168.35.46"
 //#define SERVERIP "127.0.0.1"
+//#define SERVERIP "172.20.10.3"
 
 #define SERVERPORT 9000
 #define BUFSIZE 512
